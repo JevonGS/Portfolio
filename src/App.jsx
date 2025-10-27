da76415a5395ff4d6bb485ebd3f1e2de27b85a27
JevonGS <jevonsetiawan2005@gmail.com>
2025-10-27 14:21:22 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
                 <h1 className='xl:text-6xl text-4xl font-bold mb-6' data-aos="fade-up">Hello, I'm Jevon Geraldo Setiawan</h1>
                 <p className='xl:text-xl text-base text-gray-200 opacity-50 md:mt-5 lg:mt-15' data-aos="fade-up">Student at Bina Nusantara University majoring in Computer Science. I am interested in this field because I have an interest in various areas of technology and enjoy discovering new things. My ability to adapt and my curiosity drive me to continue learning, developing my skills, and facing challenges with creative solutions.</p>
                 <div data-aos="fade-up" className='flex md:justify-between gap-5 md:mt-5 lg:mt-15 mt-10' style={{width:'100%'}}>
-                  <a href="/public/CV - Jevon Geraldo Setiawan.pdf" download className='w-full'><button className='bg-white text-gray-900 w-full rounded-xl text-base font-bold cursor-pointer hover:bg-transparent hover:text-white' style={{padding: 20, border:"solid white 1px"}}>Download CV</button></a>
+                  <a href="/CV-Jevon.pdf" download className='w-full'><button className='bg-white text-gray-900 w-full rounded-xl text-base font-bold cursor-pointer hover:bg-transparent hover:text-white' style={{padding: 20, border:"solid white 1px"}}>Download CV</button></a>
                   <a href="#Project" className='w-full'><button className='bg-gray-900 w-full rounded-xl text-base font-bold hover:bg-white hover:text-black cursor-pointer' style={{padding: 20, border:"solid white 1px"}}>Project </button></a>
                 </div>
               </div>
